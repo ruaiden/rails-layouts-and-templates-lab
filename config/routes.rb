9a8347253f24eaa0dd59038fda0744478fc192dc
Aiden <aiden.marques94@gmail.com>
2021-04-10 13:45:40 -0400

    commit tree 4fb31de4171a349f7891132f1ed569cde8ce2597

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # get 'store_admin_controller/admin'
+
+  # get 'store_admin_controller/orders'
+
+  # get 'static_controller/home'
+
   get 'home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
   get 'admin/invoice', to: 'store_admin#invoice'
+
 end
